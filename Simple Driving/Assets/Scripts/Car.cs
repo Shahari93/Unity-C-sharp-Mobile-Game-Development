aef340089d199be72b6ae5c650cd156f38b3ae8e
Shahari93 <shaharilani93@gmail.com>
2021-08-04 22:51:41 +0300

    Basic car behavior
    Car movement
    Camera Follow

---
+++ Simple Driving/Assets/Scripts/Car.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private float carSpeed = 10f;
+    [SerializeField] private float changeSpeedOverTime = 0.1f;
+    [SerializeField] private float turnSpeed = 200f;
+    private int turnValue;
+
+    private void Update() {
+        carSpeed+=changeSpeedOverTime * Time.deltaTime; // change the car speed each second (frame independent)
+        transform.Rotate(0f,turnValue * turnSpeed * Time.deltaTime, 0f);
+        transform.Translate(Vector3.forward * carSpeed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        turnValue = value;
+    }
+}
